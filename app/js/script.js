57c4c08a3e86cd5012df5f90545f7b827201c5fb
Sergey Tatsiak <tatsiak@gmail.com>
2018-06-19 01:20:53 +0300

    developed drag and drop v1.0

--- app/js/script.js
+++ app/js/script.js
@@ -3,7 +3,9 @@
 var catTemplate = document.querySelector("#cat-template"),
   catsContainer = document.querySelector(".container.cats"),
   content = document.querySelector(".content"),
-  filterElement = document.querySelector('.filter'),
+  filterElement = document.querySelector(".filter"),
+  headerCart = document.querySelector(".header__cart"),
+  cartItems = headerCart.querySelector(".header__cart_items"),
   cats,
   isBuilding = 0,
   pageCount = 0,
@@ -15,45 +17,89 @@ function getCatsData() {
   xmlHttp.open("GET", url, false);
   xmlHttp.send(null);
   pageCount++;
-  var catsObject = JSON.parse(xmlHttp.responseText)
+  var catsObject = JSON.parse(xmlHttp.responseText);
   return catsObject;
 }
 
 function getCatMarkup(data) {
   var clone = document.importNode(catTemplate.content, true);
   clone.querySelector(".cat-card__price").textContent = data.price;
-  clone.querySelector(".cat-card__image > img").src = Modernizr.svg ? data.img_url: 'img/default_cat.png';
+  clone.querySelector(".cat-card__image > img").src = Modernizr.svg ? data.img_url : "img/default_cat.png";
   clone.querySelector(".cat-card__image > img").setAttribute("alt", data.name);
   clone.querySelector(".cat-data__name").textContent = data.name;
   clone.querySelector(".cat-data__id").textContent = data.id;
   clone.querySelector(".cat-data__category").textContent = data.category;
+  clone.querySelector(".cat").id = data.id;
   return clone;
 }
 
 (function printCats() {
   var data = getCatsData();
-  data.cats.forEach(function(cat){
-    if(!filterArray.includes(cat.category)) filterArray.push(cat.category)
+  data.cats.forEach(function(cat) {
+    if (!filterArray.includes(cat.category)) filterArray.push(cat.category);
     catsContainer.appendChild(getCatMarkup(cat));
-  })
-  filterArray.forEach(function(item){
-    var button = document.createElement('button');
+  });
+  filterArray.forEach(function(item) {
+    var button = document.createElement("button");
     button.innerHTML = item;
     button.classList.add(item);
-    filterElement.addEventListener('click', filterTrigger)
+    filterElement.addEventListener("click", filterTrigger);
     filterElement.appendChild(button);
-  })
+  });
 })();
 
-cats = catsContainer.querySelectorAll('.cat');
+cats = catsContainer.querySelectorAll(".cat");
 
-function filterTrigger(e){
+cats.forEach(function(cat) {
+  cat.addEventListener("dragstart", onDrag);
+});
+headerCart.addEventListener("dragover", onDragOver);
+headerCart.addEventListener("drop", onDrop);
+
+function filterTrigger(e) {
   var clickedFilterName = e.target.classList[0];
-  e.target.classList.toggle('filter__active')
-  cats.forEach(function (cat){
-    var category = cat.querySelector('.cat-data__category').textContent
-    if (category == clickedFilterName){
-      cat.classList.toggle('hide')
+  e.target.classList.toggle("filter__active");
+  cats.forEach(function(cat) {
+    var category = cat.querySelector(".cat-data__category").textContent;
+    if (category == clickedFilterName) {
+      cat.classList.toggle("hide");
     }
-  })
+  });
+}
+
+// drag and drop
+
+function onDrag(e) {
+  e.dataTransfer.dropEffect = "move";
+  e.dataTransfer.effectAllowed = "move";
+  var target = e.target || e.srcElement;
+  var success = e.dataTransfer.setData("id", target.id);
+  console.log(success);
+}
+
+function onDragOver(e) {
+  if (e.preventDefault) e.preventDefault();
+  if (e.stopPropagation) e.stopPropagation();
+  else e.cancelBubble = true;
+  return false;
+}
+
+function addCartItem(item, id) {
+  var clone = item.cloneNode(true);
+  clone.setAttribute("data-id", id);
+  clone.removeAttribute("id");
+  clone.classList.add('header__cart_item');
+  headerCart.querySelector('.empty').classList.add('hide');
+  cartItems.appendChild(clone);
+}
+
+function onDrop(event) {
+  if (event.preventDefault) event.preventDefault();
+  if (event.stopPropagation) event.stopPropagation();
+  else event.cancelBubble = true;
+  var id = event.dataTransfer.getData("id");
+  var item = document.getElementById(id);
+  addCartItem(item, id);
+  item.classList.add('hide');
+  return false;
 }
