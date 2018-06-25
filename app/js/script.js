1a8313ba04cd8c5d56bcbdc88ce5fbbde207234d
Sergey Tatsiak <tatsiak@gmail.com>
2018-06-25 13:22:57 +0300

    rearranged js, add load more on scroll

--- app/js/script.js
+++ app/js/script.js
@@ -6,65 +6,66 @@ var catTemplate = document.querySelector("#cat-template"),
   filterElement = document.querySelector(".filter"),
   headerCart = document.querySelector(".header__cart"),
   cartItems = headerCart.querySelector(".header__cart_items"),
+  load = document.querySelector("#load"),
+  gettingData = false,
   cats,
   isBuilding = 0,
   pageCount = 1,
   filterArray = [];
 
+// build
+getCatsData();
+content.addEventListener("scroll", getCatsData);
+
 function getCatsData() {
-  var xmlHttp = new XMLHttpRequest();
-  var url = "https://ma-cats-api.herokuapp.com/api/cats?page=" + pageCount;
-  xmlHttp.open("GET", url, false);
-
-  xmlHttp.onload = function() {
-    if (xmlHttp.status >= 200 && xmlHttp.status < 400) {
-      // Success!
-      var data = JSON.parse(xmlHttp.responseText);
-      pageCount++;
-      printCats(data);
-    } else {
-      // We reached our target server, but it returned an error
-      console.error(xmlHttp.responseText);
-    }
-  };
-
-  xmlHttp.send(null);
+  if (isElementInViewport(load) && !gettingData) {
+    gettingData = true;
+    var xmlHttp = new XMLHttpRequest();
+    var url = "https://ma-cats-api.herokuapp.com/api/cats?page=" + pageCount;
+    xmlHttp.open("GET", url, false);
+    xmlHttp.onload = function() {
+      if (xmlHttp.status >= 200 && xmlHttp.status < 400) {
+        // Success!
+        var data = JSON.parse(xmlHttp.responseText);
+        pageCount++;
+        data.cats.forEach(function(cat) {
+          catsContainer.appendChild(getCatMarkup(cat));
+          if (!filterArray.includes(cat.category)) {
+            filterArray.push(cat.category);
+            var button = document.createElement("button");
+            button.innerHTML = cat.category;
+            button.classList.add(cat.category);
+            filterElement.addEventListener("click", filterTrigger);
+            filterElement.appendChild(button);
+          }
+        });
+        cats = catsContainer.querySelectorAll(".cat");
+        cats.forEach(function(cat) {
+          cat.addEventListener("dragstart", onDrag);
+        });
+      } else {
+        // We reached our target server, but it returned an error
+        console.error(xmlHttp.responseText);
+      }
+    };
+    xmlHttp.send(null);
+    gettingData = false;
+  }
 }
 
 function getCatMarkup(data) {
   var clone = document.importNode(catTemplate.content, true);
   clone.querySelector(".cat-card__price").textContent = data.price;
-  var imageSource = Modernizr.svg ? data.img_url : 'img/default_cat.png';
-  clone.querySelector(".cat-card__image").setAttribute('style', 'background-image: url("' + imageSource +'")')
+  var imageSource = Modernizr.svg ? data.img_url : "img/default_cat.png";
+  clone
+    .querySelector(".cat-card__image")
+    .setAttribute("style", 'background-image: url("' + imageSource + '")');
   clone.querySelector(".cat-data__name").textContent = data.name;
   clone.querySelector(".cat-data__id").textContent = data.id;
   clone.querySelector(".cat-data__category").textContent = data.category;
   clone.querySelector(".cat").id = data.id;
   return clone;
 }
-
-function printCats(data) {
-  data.cats.forEach(function(cat) {
-    if (!filterArray.includes(cat.category)) filterArray.push(cat.category);
-    catsContainer.appendChild(getCatMarkup(cat));
-  });
-  filterArray.forEach(function(item) {
-    var button = document.createElement("button");
-    button.innerHTML = item;
-    button.classList.add(item);
-    filterElement.addEventListener("click", filterTrigger);
-    filterElement.appendChild(button);
-  });
-};
-
-getCatsData()
-cats = catsContainer.querySelectorAll(".cat");
-
-cats.forEach(function(cat) {
-  cat.addEventListener("dragstart", onDrag);
-});
-headerCart.addEventListener("dragover", onDragOver);
-headerCart.addEventListener("drop", onDrop);
 
 function filterTrigger(e) {
   var clickedFilterName = e.target.classList[0];
@@ -77,7 +78,20 @@ function filterTrigger(e) {
   });
 }
 
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.bottom > 0 &&
+    rect.right > 0 &&
+    rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
+    rect.top < (window.innerHeight || document.documentElement.clientHeight)
+  );
+}
+
 // drag and drop
+
+headerCart.addEventListener("dragover", onDragOver);
+headerCart.addEventListener("drop", onDrop);
 
 function onDrag(e) {
   e.dataTransfer.dropEffect = "move";
@@ -98,8 +112,8 @@ function addCartItem(item, id) {
   var clone = item.cloneNode(true);
   clone.setAttribute("data-id", id);
   clone.removeAttribute("id");
-  clone.classList.add('header__cart_item');
-  headerCart.querySelector('.empty').classList.add('hide');
+  clone.classList.add("header__cart_item");
+  headerCart.querySelector(".empty").classList.add("hide");
   cartItems.appendChild(clone);
 }
 
@@ -110,6 +124,6 @@ function onDrop(e) {
   var id = e.dataTransfer.getData("id");
   var item = document.getElementById(id);
   addCartItem(item, id);
-  item.classList.add('hide');
+  item.classList.add("hide");
   return false;
 }
